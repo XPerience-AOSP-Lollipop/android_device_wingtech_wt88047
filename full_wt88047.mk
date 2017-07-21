1aa665bbfdd1587796fc9a45e9ebc815273a28a6
klozz (TeamMEX) <xXx.Reptar.Rawrr.xXx@gmail.com>
2017-07-21 05:08:34 -0500

    fix more related to device rename

--- full_wt88047.mk
+++ full_wt88047.mk
@@ -18,7 +18,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from wt88047 device
-$(call inherit-product, device/wingtech/wt88047/device.mk)
+$(call inherit-product, device/xiaomi/wt88047/device.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := wt88047
