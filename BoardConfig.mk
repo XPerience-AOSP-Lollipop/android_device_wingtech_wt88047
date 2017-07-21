516b6bcc2bd187774a7435c8587f3fc9caefe1c3
klozz (TeamMEX) <xXx.Reptar.Rawrr.xXx@gmail.com>
2017-07-21 05:07:28 -0500

    fix that shit

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -17,7 +17,7 @@ FORCE_32_BIT := true
 
 include device/cyanogen/msm8916-common/BoardConfigCommon.mk
 
-DEVICE_PATH := device/wingtech/wt88047
+DEVICE_PATH := device/xiaomi/wt88047
 include $(DEVICE_PATH)/board/*.mk
 
 # inherit from the proprietary version
